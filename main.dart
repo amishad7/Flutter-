b35d8038fa5ec99649b7cdf41fb09f6f304661d0
amishad7 <118448879+amishad7@users.noreply.github.com>
2023-03-21 18:48:47 +0530

    Task_1

---
+++ main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+void main()
+{
+  runApp(MaterialApp(
+  title: "demo app",
+    debugShowCheckedModeBanner: false,
+    home: SafeArea(child: Text("hello\n\n\n\ndart\n\n\n\nflutter")),
+  ));
+
+}
