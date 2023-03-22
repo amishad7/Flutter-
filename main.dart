5d5b6b96e123f4eaa703c0504a2390e55a14b0b7
amishad7 <118448879+amishad7@users.noreply.github.com>
2023-03-22 11:22:00 +0530

    Task_3

--- main.dart
+++ main.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
-void main()
-{
-  runApp(MaterialApp(
-  title: "demo app",
-    debugShowCheckedModeBanner: false,
-    home: SafeArea(child: Text("hello\n\n\n\ndart\n\n\n\nflutter")),
-  ));
+import 'dart:ui';
 
+void main() {
+  runApp(
+    MaterialApp(
+      title: "Demo App",
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          leading: Icon(Icons.menu),
+          title: Text("Flutter App"),
+          backgroundColor: Color(0XFFffc107),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Text(
+            "Red & White",
+            style: TextStyle(
+                fontSize: 57,
+                color: Color.fromRGBO(255, 34, 4, 0.90),
+                decoration: TextDecoration.underline,
+                decorationStyle: TextDecorationStyle.double,
+                decorationColor: Color.fromRGBO(215, 196, 22, 0.8)),
+          ),
+        ),
+      ),
+    ),
+  );
 }
